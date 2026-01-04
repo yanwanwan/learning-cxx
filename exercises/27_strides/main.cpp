d6f8dd9f3c1b2995bc38aa60efd2c9def7386d30
Yanwanwan <1078207549@qq.com>
2026-01-04 21:14:52 +0800

    27 28

--- exercises/27_strides/main.cpp
+++ exercises/27_strides/main.cpp
@@ -18,6 +18,17 @@ std::vector<udim> strides(std::vector<udim> const &shape) {
     // TODO: 完成函数体，根据张量形状计算张量连续存储时的步长。
     // READ: 逆向迭代器 std::vector::rbegin <https://zh.cppreference.com/w/cpp/container/vector/rbegin>
     //       使用逆向迭代器可能可以简化代码
+    udim product = 1;
+    auto it_shape = shape.rbegin();
+    auto it_stride = strides.rbegin();
+    while (it_shape != shape.rend()) {
+        *it_stride = product;
+
+        product = product * (*it_shape);
+
+        it_shape++;
+        it_stride++;
+    }
     return strides;
 }
 
